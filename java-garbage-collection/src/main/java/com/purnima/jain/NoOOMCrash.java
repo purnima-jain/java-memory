eaa25afee30c442a8e4aa355dc19806335919e48
Purnima Jain <jain_purnima@hotmail.com>
2025-07-23 13:31:33 +0530

    Add more commands and scenarios

--- java-garbage-collection/src/main/java/com/purnima/jain/NoOOMCrash.java
+++ java-garbage-collection/src/main/java/com/purnima/jain/NoOOMCrash.java
@@ -3,7 +3,7 @@ package com.purnima.jain;
 import java.util.HashMap;
 import java.util.Map;
 
-public class OOMCrash {
+public class NoOOMCrash {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Application started.......");
@@ -17,10 +17,12 @@ public class OOMCrash {
 		while (true) {
 
 			counter++;
-			map.put("key-" + counter, "value-" + counter); // Keep adding entries to the map infinitely
+			map.put("key-" + counter, "value-" + counter); // Keep adding entries to the map
 
 			if (counter % 10000 == 0) {
 				System.out.println("Added " + counter + " elements");
+				System.out.println("Removing " + counter + " elements");
+				map = new HashMap<>(); // Allocate new Map instance
 				Thread.sleep(2000);
 			}
 		}
--- java-garbage-collection/startApp.bat
+++ java-garbage-collection/startApp.bat
@@ -1,17 +1,27 @@
+call setJavaEnv.bat
 call compileApp.bat
 
 
-REM ******************************************
+REM ************************************************************************************
 
 
-REM ***************** Displays command line options with their values ****************
-REM %JAVA% -XX:+PrintCommandLineFlags -version
+echo ************************************************************************************
 
+REM ***************** Displays the most useful flags controlling the heap size and garbage collection along with their values.****************
+%JAVA% -XX:+PrintCommandLineFlags -version
+
+echo ************************************************************************************
+
+REM ***************** Displays all the global JVM tuning flags ***************** 
+REM %JAVA% -XX:+PrintFlagsFinal -version
+
+echo ************************************************************************************
 
 REM **************** Generates GC Log ****************
 REM %JAVA% -Xms10m -Xmx20m -Xlog:gc*:file=.\garbage\gc.log -cp ./target/java-garbage-collection-0.0.1-SNAPSHOT.jar com.purnima.jain.NoOOMCrash
 
+echo ************************************************************************************
 
 REM **************** Triggers Out of Memory Error due to Heap Space ****************
-REM %JAVA% -Xms10m -Xmx20m -verbose:gc -cp ./target/java-garbage-collection-0.0.1-SNAPSHOT.jar com.purnima.jain.OOMCrash
+REM %JAVA% -Xms10m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError -XX:+UseGCOverheadLimit -cp ./target/java-garbage-collection-0.0.1-SNAPSHOT.jar com.purnima.jain.OOMCrash
 
